a0105e9af621560020d1ac2d6069e124f358e0e7
Lakshith Vithana <lakshithlfvithana@gmail.com>
2023-07-18 10:53:19 +0530

    notification page added

---
+++ lib/features/chat/data/data_provider/chat_data_provider_impl.dart
@@ -0,0 +1,10 @@
+import 'package:dartz/dartz.dart';
+import 'package:seniorcompanion/features/chat/data/data_provider/chat_data_provider.dart';
+
+class ChatDataProviderImpl implements ChatDataProvider {
+  @override
+  Future<Unit> createChat({required String myUid, required String partnerUid}) {
+    // TODO: implement createChat
+    throw UnimplementedError();
+  }
+}
